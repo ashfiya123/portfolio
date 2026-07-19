9f85225b67eb9bd3605b7413860a7ea3b9bed97a
Ashfiya <asfiyashekh30@gmail.com>
2026-07-19 16:48:21 +0530

    Fixed contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,50 +4,41 @@ import "./styles/Contact.scss";
 const Contact = () => {
   return (
     <section id="contact" className="contact">
-
-    
-
       <h2>Contact Me</h2>
 
-     <div className="contact-card">
+      <div className="contact-card">
+        <h3>Phone Number</h3>
+        <a href="tel:7455048206">
+          7455048206
+        </a>
 
-      <h3>Phone number</h3>
+        <h3>Email</h3>
+        <a
+          href="mailto:asfiyashekh30@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          asfiyashekh30@gmail.com
+        </a>
 
-  <a
-    target="_blank"
-    rel="noreferrer"
-  >
-   7455048206
-  </a>
-         <h3>Email</h3>
+        <h3>LinkedIn</h3>
+        <a
+          href="https://linkedin.com/in/ashfiya-76a626283"
+          target="_blank"
+          rel="noreferrer"
+        >
+          View LinkedIn Profile
+        </a>
 
-  <a
-    href="asfiyashekh30@gmail.com"
-    target="_blank"
-    rel="noreferrer"
-  >
-    asfiyashekh30@gmail.com
-  </a>
-  <h3>LinkedIn</h3>
-
-  <a
-    href="https://linkedin.com/in/ashfiya-76a626283"
-    target="_blank"
-    rel="noreferrer"
-  >
-    View LinkedIn Profile
-  </a>
-
-  <h3>GitHub</h3>
-
-  <a
-    href="https://github.com/ashfiya123"
-    target="_blank"
-    rel="noreferrer"
-  >
-    github.com/ashfiya123
-  </a>
-</div>
+        <h3>GitHub</h3>
+        <a
+          href="https://github.com/ashfiya123"
+          target="_blank"
+          rel="noreferrer"
+        >
+          github.com/ashfiya123
+        </a>
+      </div>
     </section>
   );
 };
